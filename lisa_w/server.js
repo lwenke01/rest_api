43dab73f5a74eabff218d29dd1985b27ff1fd83b
lwenke01 <lwenke@gmail.com>
2016-04-28 11:34:01 -0700

    getting auth to work

--- lisa_w/server.js
+++ lisa_w/server.js
@@ -4,13 +4,13 @@ const mongoose = require('mongoose');
 const express = require('express');
 const app = module.exports = exports = express();
 
-const port = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 const DB_PORT = process.env.MONGOLAB_URI || 'mongodb://localhost/db';
 mongoose.connect(DB_PORT);
 
 app.use((req, res, next)=>{
-  res.header('Access-Control-Allow-Origin', 'http://localhost:6000');
+  res.header('Access-Control-Allow-Origin', 'http://localhost:5000');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, token');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   next();
@@ -26,6 +26,6 @@ app.use(gameRouter);
 app.use(userRouter);
 app.use(authRouter);
 
-app.listen(port, ()=>{
-  console.log('Magic is happening on port ' + port);
+app.listen(PORT, ()=>{
+  console.log('Magic is happening on port ' + PORT);
 });
